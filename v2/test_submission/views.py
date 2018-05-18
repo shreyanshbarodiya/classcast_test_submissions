8ff7b17d863e606a3f6b6f4c088c194a8d17ab18
shreyansh <shreyansh.barodiya@gmail.com>
2018-05-18 16:13:44 +0530

    small changes

--- v2/test_submission/views.py
+++ v2/test_submission/views.py
@@ -13,7 +13,7 @@ def allsubmissions(request, user_id):
 	res = models.Classcast_test_submission.objects.filter(student_id=user_id)
 	# res = api.get_all_student_test_submission(user_id)
 
-	return JsonResponse(res)
+	return JsonResponse(res, safe=False)
 
 
 def newsubmission(request, user_id):
