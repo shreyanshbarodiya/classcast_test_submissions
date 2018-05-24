9ffc94e0f363d51bb27a03ea06aa395939ef138b
shreyansh <shreyansh.barodiya@gmail.com>
2018-05-24 16:35:47 +0530

    bookkeeping

--- v2/urls.py
+++
@@ -1,9 +0,0 @@
-"""
-test submission API URI specification.
-"""
-from django.conf.urls import include, url, patterns
-
-urlpatterns = patterns(
-    '',
-    url(r'^', include('classcast_test_submissions.v2.test_submission.urls')),
-)
